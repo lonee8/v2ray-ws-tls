9d5c5790217c49922f90e0f8fbd3cb0a216c3fb1
CN_SZTL <22235437+1715173329@users.noreply.github.com>
2018-03-18 16:28:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+sudo su
 php-fpm7.0 --daemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
 sed -i "s/CLIENT_ID/$CLIENT_ID/g" /etc/v2ray/config.json
 sed -i "s/CLIENT_ALTERID/$CLIENT_ALTERID/g" /etc/v2ray/config.json
-sed -i "s/CLIENT_SECURITY/$CLIENT_SECURITY/g" /etc/v2ray/config.json
 v2ray -config=/etc/v2ray/config.json &
 nginx -g 'daemon off;'
